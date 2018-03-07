845e321f52c85bb28dba79af575c057149e69a01
hmadan <hmadan@ei6x>
2018-03-07 13:48:53 +0900

    inz

--- qtcmd.bat
+++ qtcmd.bat
@@ -1,7 +1,8 @@
 @echo off
-call prepareqt.bat
+call %~dp0prepareqt.bat
 set PATH=%QTROOT%\5.10.0\mingw53_32\bin;%PATH%
 set PATH=%QTROOT%\Tools\mingw530_32\bin;%PATH%
 
 echo Qt is inserted in path.
+@echo on
 cmd /k
