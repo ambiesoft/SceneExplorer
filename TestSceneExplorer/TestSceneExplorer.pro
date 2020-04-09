cc31a4df682f4cc56a51e46d248f3ed933138d37
ambiesoft <ambiesoft.trueff@gmail.com>
2020-04-09 10:45:36 +0900

    Add googletest

--- TestSceneExplorer/TestSceneExplorer.pro
+++ TestSceneExplorer/TestSceneExplorer.pro
@@ -1,7 +1,43 @@
 TEMPLATE = app
 CONFIG += console c++11
 CONFIG -= app_bundle
-CONFIG -= qt
+CONFIG += qt
+QT += core gui
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 SOURCES += \
+        ../../lsMisc/stdQt/stdQt.cpp \
+        googletest/googletest/src/gtest-all.cc \
+        googletest/googletest/src/gtest.cc \
+        googletest/googletest/src/gtest_main.cc \
         main.cpp
+
+INCLUDEPATH += googletest/googletest/include \
+        googletest/googletest/
+
+HEADERS += \
+    ../../lsMisc/stdQt/stdQt.h \
+    ../../lsMisc/stdafx.h
+
+
+win32 {
+    message("win32")
+    HEADERS += \
+        ../../lsMisc/GetLastErrorString.h
+
+    SOURCES += \
+        ../../lsMisc/stdQt/stdQt_win32.cpp \
+        ../../lsMisc/GetLastErrorString.cpp
+
+    win32-g++ {
+        message("win32-g++")
+        LIBS += -lshlwapi -lPsapi
+    }
+}
+linux {
+    message("linux-g++")
+    SOURCES += \
+        ../../lsMisc/stdQt/stdQt_linux.cpp
+}
+
+
--- TestSceneExplorer/main.cpp
+++ TestSceneExplorer/main.cpp
@@ -1,9 +1,31 @@
-#include <iostream>
+#include <QString>
 
-using namespace std;
+#include "gtest/gtest.h"
 
-int main()
+#include "../../lsMisc/stdQt/stdQt.h"
+
+using namespace AmbiesoftQt;
+
+#define EXPECT_QSTREQ(left,right) EXPECT_STREQ(QString(left).toUtf8(), QString(right).toUtf8())
+
+TEST(stdQt,RemoveExtensionFromPath)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    QString path = "";
+    EXPECT_TRUE(RemoveExtensionFromPath(nullptr).isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath("").isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath(" ").isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath(".").isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath(" . ").isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath(".aaa").isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath("..aaa").isEmpty());
+    EXPECT_TRUE(RemoveExtensionFromPath(" ..aaa").isEmpty());
+
+
+    EXPECT_QSTREQ(RemoveExtensionFromPath("aaa"),"aaa");
+    EXPECT_QSTREQ(RemoveExtensionFromPath("/a/a"),"a");
+    EXPECT_QSTREQ(RemoveExtensionFromPath("/a/a.b..") ,"a.b");
+    EXPECT_QSTREQ(RemoveExtensionFromPath("C:\\aaa\bbb\\") ,"");
+    EXPECT_QSTREQ(RemoveExtensionFromPath("C:\\aaa\bbb\\t t t") ,"t t t");
+    EXPECT_QSTREQ(RemoveExtensionFromPath("C:\\aaa\bbb\\t t t ") ,"");
+    EXPECT_QSTREQ(RemoveExtensionFromPath("C:\\aaa\bbb\\t t t.mp4") ,"t t t");
 }
