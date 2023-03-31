22d77f48a7ce2afcc812c33ad7b25ed2ed88f213
ambiesoft <ambiesoft.trueff@gmail.com>
2023-03-31 15:53:52 +0900

    added bat

---
+++ startvsproj2022-32.bat
@@ -0,0 +1,11 @@
+@echo off
+set DEL=sdel
+set VCVARSBAT="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+set DEVENV="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+
+call %~dp0prepare.bat
+set VSQTVER=%QTVERSION%
+set VSQTTOOL=msvc2019
+
+
+funcvsproj.bat
